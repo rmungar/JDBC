d398be1e1c99cada5ec457b81fc9b5f4515b3bf4
rmungar1209 <rmungar1209@g.educaand.es>
2024-05-09 13:33:32 +0200

    hola

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,18 @@
 package org.example
 
 import org.example.DAO.ProductDAO
+import org.example.DSF.DataSourceFactory
 import org.example.ENTITY.Product
+import org.example.OUTPUT.Console
+import javax.sql.DataSource
 
 fun main() {
+    val console = Console()
+    val dataSource = DataSourceFactory.getDS(DataSourceFactory.DataSourceType.HIKARI)
 
     val product = Product(1, "Smartphone", 999.99f, "The latest smartphone model", "Apple", "Electronics")
 
-    val productId = ProductDAO().createProduct(product)
+    val productId = ProductDAO(dataSource, console).createProduct(product)
 
     println("Product ID: $productId")
 
